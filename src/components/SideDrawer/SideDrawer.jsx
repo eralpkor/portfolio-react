d88196951aaf3005eecba0282dfcc24e17aff13f
eralpkor <eralp.ny@gmail.com>
2020-02-26 12:53:33 -0500

    navbar, sidedrawer workde on css

--- src/components/SideDrawer/SideDrawer.jsx
+++ src/components/SideDrawer/SideDrawer.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './SideDrawer.css';
 
-const sideDrawer = props => (
-  <nav className='side-drawer'>
-    <div className='toolbar-navigation-items'>
-      <Link to='/about'>About</Link>
-      <Link to='projects'>Projects</Link>
-      <Link to='contact'>Contact</Link>
-      <a href='https://blog.eralpkor.com/'>Blog</a>
-    </div>
-  </nav>
-);
+const sideDrawer = props => {
+  // console.log(props);
+  let drawerClasses = "side-drawer";
+
+  if (props.show) {drawerClasses = "side-drawer open"}
+
+  return (
+    <nav className={drawerClasses}>
+      <div className='toolbar-navigation-items'>
+        <Link to='/'>About</Link>
+        <Link to='projects'>Projects</Link>
+        <Link to='contact'>Contact</Link>
+        <a href='https://blog.eralpkor.com/'>Blog</a>
+      </div>
+    </nav>
+  )
+  
+};
 
 export default sideDrawer;
